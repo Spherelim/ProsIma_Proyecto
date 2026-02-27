abdd35c2539a58e13302e46fd8af6dc4e436b14a
Spherelim <mau.ortiz350@gmail.com>
2026-02-26 20:33:06 -0600

    script(threeSetup.js,RA_Ready.html):Aplicar Rotacion a la camara, Agregar Coordenadas de la Camara y Cubo,Agregar Escala con escroll

--- Public/scripts/RA_Testings/threeSetup.js
+++ Public/scripts/RA_Testings/threeSetup.js
@@ -1,6 +1,8 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-
+//import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const container = document.querySelector(".phone-camera");
+const infoPanel = document.querySelector("#coords-info");
 
 // Escena
 const scene = new THREE.Scene();
@@ -13,10 +15,10 @@ const camera = new THREE.PerspectiveCamera(
     1000
 );
 
-camera.position.z = 3;
+camera.position.z = 5;
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({ alpha: true });
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(container.clientWidth, container.clientHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
@@ -28,12 +30,35 @@ const material = new THREE.MeshNormalMaterial();
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+container.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    const scaleAmount = event.deltaY * -0.001;
+
+    cube.scale.x = Math.max(0.1, cube.scale.x + scaleAmount);
+    cube.scale.y = Math.max(0.1, cube.scale.y + scaleAmount);
+    cube.scale.z = Math.max(0.1, cube.scale.z + scaleAmount);
+
+},{passive: false});
+
 // 🔄 Animación
 function animate(){
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    controls.update();
+
+    if(infoPanel){
+        infoPanel.innerHTML = `
+            <b>Camera Position:</b> (${camera.position.x.toFixed(2)}, ${camera.position.y.toFixed(2)}, ${camera.position.z.toFixed(2)})<br>
+            <b>cubo escala:</b> ${cube.scale.x.toFixed(2)}
+        `
+
+    }
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
